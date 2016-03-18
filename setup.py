d9ffcd0053c9f54fa8cdf933cc77d303ea520f8c
Sylvain Viollon <sviollon@minddistrict.com>
2016-03-18 10:05:29 +0100

    Back to development: 1.4.7.post1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 # For example, a packaging bugfix release version 1.4.4 of the
 # js.jquery package would be version 1.4.4-1 .
 
-version = '1.4.7'
+version = '1.4.7.post1.dev0'
 
 
 def read(*rnames):
